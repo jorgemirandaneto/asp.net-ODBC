810093b4ddfa462f24147f5d1375f974dee265fb
Jorge Miranda <jorge.miranda@sam.fortaleza.ce.gov.br>
2017-05-19 16:50:07 -0300

    Atualização

---
+++ ODBC/InsereEditora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using Npgsql;
+
+namespace ODBC
+{
+    class InsereEditora
+    {
+        static void Main(string[] args)
+        {
+            string stringDeConexao = @"Server=127.0.0.1;Port=5432;User Id=postgres;Password=1q2w3e4r;Database=teste;";
+
+            
+            Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu email");
+            string email = Console.ReadLine();
+
+            NpgsqlConnection conexao = new NpgsqlConnection(stringDeConexao);
+
+            string textoInsereEditora = @"insert into editora (nome, email) values ('?','?')";
+
+            NpgsqlCommand comando = new NpgsqlCommand(textoInsereEditora, conexao);
+
+            comando.Parameters.AddWithValue(@"nome", nome);
+
+            comando.Parameters.AddWithValue(@"email", email);
+
+            conexao.Open();
+
+            comando.ExecuteNonQuery();
+
+            conexao.Close();
+        }
+    }
+}
---
+++ ODBC/Listar.cs
@@ -0,0 +1,38 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODBC
+{
+    class Listar
+    {
+        static void Main(string[] args)
+        {
+            string stringDeConexao = @"Server=127.0.0.1;Port=5432;User Id=postgres;Password=1q2w3e4r;Database=teste;";
+
+            NpgsqlConnection conexao = new NpgsqlConnection(stringDeConexao);
+
+
+
+            string listar = @"select * from editora";
+
+            NpgsqlCommand comando = new NpgsqlCommand(listar, conexao);
+
+            conexao.Open();
+
+            NpgsqlDataReader resultado = comando.ExecuteReader();
+
+            while (resultado.Read())
+            {
+                string nome = resultado["nome"] as string;
+                string email = resultado["email"] as string;
+
+                Console.WriteLine("Nome: " + nome + "   Email: " + email +"\n");
+            }
+        }
+    }
+}
+
